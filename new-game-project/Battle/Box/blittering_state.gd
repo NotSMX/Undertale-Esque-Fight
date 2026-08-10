40680512cc72b2ad9149cb1548227920d21310d6
SMX <dannyyu12@gmail.com>
2026-08-10 17:35:16 -0400

    New Button Sprites

--- new-game-project/Battle/Box/blittering_state.gd
+++ new-game-project/Battle/Box/blittering_state.gd
@@ -1,10 +1,10 @@
 extends BattleBoxBehaviour
 
 var action_text := {
-	0: "* You ready your weapon.",
-	1: "* You check yourself.",
-	2: "* You check your items.",
-	3: "* You plead for mercy.",
+	0: "* Select your target.",
+	1: "* What will you do?",
+	2: "* What do you need?",
+	3: "* They will not let you.",
 }
 
 func _on_gain_control() -> void:
